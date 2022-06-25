9444c744c00bda3c202f4cad02a971e0211a548f
Carolina Ferreiro Belenguer <78199213+CarolinaFerre@users.noreply.github.com>
2022-06-25 18:02:52 +0200

    formulario y axios terminado

--- proyect/src/components/FormularioAsistencia.vue
+++ proyect/src/components/FormularioAsistencia.vue
@@ -14,43 +14,46 @@
                 />
         </div>
         <div class="formulario">
-            <form>
+            <form v-on:submit.prevent="recibirFormulario()">
                 <div class="row g-3 align-items-center">
                     <div class="col-auto">
                         <label for="nombre" class="form-label">Nombre</label>
-                        <input type="text" class="form-control" id="nombre" width="10" heigth="10" placeholder="Debes rellenar los campos"/>
+                        <input type="text" class="form-control" id="nombre" required="required" placeholder="Debes rellenar los campos" v-model="user.nombre"/>
                     </div>
                 </div>
-                
+                <br>
                 <div class="row g-3 align-items-center">
                     <div class="col-auto">
-                        <label for="confirmacion" class="form-label">Confirmación</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1"/>
+                        <label class="form-check-label" for="confirmacion">Confirmación</label>&nbsp;&nbsp;&nbsp;&nbsp;
+                        Sí &nbsp;<input class="form-check-input" type="radio" name="confirmacion" id="confirmacion" value="si" required="required"  v-model="user.confirmacion">
+                        No &nbsp;<input class="form-check-input" type="radio" name="confirmacion" id="confirmacion" value="no"  required="required" v-model="user.confirmacion">
                     </div>
                 </div>
+                <br>
                 <div class="row g-3 align-items-center">
                     <div class="col-auto">
                         <label class="form-check-label" for="bus">¿Necesitas autobús?</label>&nbsp;&nbsp;&nbsp;&nbsp;
-                        Sí &nbsp;<input class="form-check-input" type="radio" name="bus" id="bus" value="si">
-                        No &nbsp;<input class="form-check-input" type="radio" name="bus" id="bus" value="no">
+                        Sí &nbsp;<input class="form-check-input" type="radio" name="bus" id="bus" value="si" required="required" v-model="user.bus">
+                        No &nbsp;<input class="form-check-input" type="radio" name="bus" id="bus" value="no"  required="required"  v-model="user.bus">
                     </div>
                 </div>
+                <br>
                 <div class="row g-3 align-items-center">
                     <div class="col-auto">
                         <label class="form-check-label" for="alergias">Alergias</label>&nbsp;&nbsp;&nbsp;&nbsp;
-                        Sí &nbsp;<input class="form-check-input" type="radio" name="alergias" id="alergias" value="si">
-                        No &nbsp;<input class="form-check-input" type="radio" name="alergias" id="alergias" value="no">
+                        Sí &nbsp;<input class="form-check-input" type="radio" name="alergias" id="alergias" value="si" required="required" v-model="user.alergias">
+                        No &nbsp;<input class="form-check-input" type="radio" name="alergias" id="alergias" value="no" required="required"  v-model="user.alergias">
                     </div>
                 </div>
+                <br>
                 <div class="row g-3 align-items-center">
                     <div class="col-auto">
                         <label class="form-label" for="comertarios">Comentarios</label>
-                        <textarea class="form-control" placeholder="Debes rellenar los campos" id="comentarios"></textarea>
+                        <textarea class="form-control" placeholder="Debes rellenar los campos" id="comentarios" required="required" v-model="user.comentarios"></textarea>
                     </div>
                 </div>
-                <div class="row g-3 align-items-center">
-                    <br>
-                </div>
+                <br>
+                <br>
                 
                 <button type="submit" class="boton boton--pink-1">Submit</button>
                 <div class="row g-3 align-items-center">
@@ -66,8 +69,55 @@
 
 </template>
 <script>
-//import swal from 'sweetalert';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+
 export default {
     name: "FormularioAsistencia",
+    data(){
+        
+        return{
+            user:{
+                nombre:'',
+                confirmacion:'',
+                bus:'',
+                alergias:'',
+                comentarios:''
+            },
+            resultados:[]
+        }
+    },
+    methods:{
+        recibirFormulario(){
+            console.log(this.user);
+            
+            axios.post('https://app-asistentes.herokuapp.com/api/save/',{
+                nombre:this.user.nombre,
+                confirmacion:this.user.confirmacion,
+                bus:this.user.bus,
+                alergias:this.user.alergias,
+                comentarios:this.user.comentarios
+        })
+        .then(res=>{
+            this.resultados=res.data;
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Tus datos han sido guardados, muchas gracias',
+                showConfirmButton: false,
+                timer: 1500,
+                width: 400,
+                padding: '3em',
+                color: '#FEF9F6',
+                background: '#171c18'
+        
+            })
+                
+                
+            })
+
+        },
+        
+    }
 };
 </script>
